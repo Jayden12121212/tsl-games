40db6b97fbac77f9368b0418c6625303fc5e8c6a
Jayden Sham <shamjayden@gmail.com>
2023-10-18 16:36:12 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,7 @@
 import React from "react";
-import { Typography, Button } from "@material-tailwind/react";
-import { Session } from "next-auth";
-import { GetServerSideProps } from "next";
-import { getSession } from "next-auth/react";
-import { signIn, signOut } from "next-auth/react";
 
-type HomeProps = {
-  session: Session | null;
-};
-
-const Home = ({ session }: HomeProps) => {
-  return (
-    <div>
-      {session ? (
-        <div>
-          <Typography variant="h3">Welcome, {session.user.name}</Typography>
-          <Button onClick={() => signOut()}>Log out</Button>
-        </div>
-      ) : (
-        <div>
-          <Typography variant="h3">Welcome to our website</Typography>
-          <Button onClick={() => signIn()}>Log in</Button>
-        </div>
-      )}
-    </div>
-  );
+const Home = () => {
+  return <h1>Home</h1>;
 };
 
 export default Home;
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const session = await getSession(context);
-
-  return {
-    props: { session },
-  };
-};
